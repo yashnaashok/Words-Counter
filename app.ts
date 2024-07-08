f563afb71c7481faf5ccf059446038ce4e220bd3
yashnaashok <168189535+yashnaashok@users.noreply.github.com>
2024-07-08 17:43:35 +0700

    Words-Counter

---
+++ app.ts
@@ -0,0 +1,27 @@
+// Importing Inquirer And Chalk Package 
+import inquirer from "inquirer";
+import chalk from "chalk";
+
+// Display A Colorfull Welcome Message
+console.log(chalk.bold.cyanBright("\n \t\t Welcome To Yashna - Word_Counter"));
+console.log("=".repeat(65));
+
+// Prompt The User To Enter A Sentence
+let answers = await inquirer.prompt([
+    {
+        name: "sentence",
+        type: "input",
+        message: "Enter A Sentence",
+    }
+]);
+
+// Trimming The Sentence And Splitting It Into Words Based On "Spaces"
+
+let words = answers.sentence.trim().split(" ");
+
+// Analysis Of User Input Sentence
+console.log("=".repeat(65));
+console.log(chalk.bold("- Sentence Words:"));
+console.log(words);
+console.log(chalk.bold(`\n -Word Count: ${chalk.bold.redBright(words.length)}`));
+console.log("=".repeat(65));
